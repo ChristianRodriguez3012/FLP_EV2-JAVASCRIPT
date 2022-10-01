0642c4f780a57c9f3a461c6bf470b4bd79563494
ChristianRodriguez3012 <103950506+ChristianRodriguez3012@users.noreply.github.com>
2022-09-30 21:10:03 -0500

    Update EJERCICIO1.js
    hubo un error al hacer commit con el primer ejercicio (para poner el codigo hice "ctrl+x" en vez de "ctrl+v") y corté el código en lugar de copiarlo.

--- EJERCICIO1.js
+++ EJERCICIO1.js
@@ -1,1 +1,16 @@
-
+const input = prompt("FIBONACCI - INGRESE EL NÚMERO A CALCULAR:");
+alert(`Calculando el fibonacci de ${input}`);
+    function CREARFibonacci(number){
+    var fibonacci = [];
+//fibonacci de 0 y 1
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+//fibonacci
+for (var i = 2; i < number; i++) {
+        fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
+    return fibonacci;
+    }
+    //output
+    var f = CREARFibonacci(input);
+    console.log(f);
